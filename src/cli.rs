8ebc430c18dc43d928647b555a15e93806e458af
jhzhics <zhangjiahao2022@stu.pku.edu.cn>
2025-03-09 07:14:12 +0000

    feat: implement lexer

---
+++ src/cli.rs
@@ -0,0 +1,93 @@
+use std::{env, process::exit};
+
+#[derive(Debug)]
+pub enum Mode {
+    /// Generate Koopa IR
+    Koopa,
+    /// Generate RISC-V assembly
+    Riscv,
+    /// Performance test
+    Perf
+}
+pub struct Cli {
+    pub mode: Mode,
+    /// Input file
+    /// If not specified, read from stdin
+    pub input_file: Option<String>,
+    /// Output file
+    /// If not specified, write to stdout
+    pub output_file: Option<String>,
+}
+
+const USAGE: &str = r#"Usage: 
+SysY [options] input_file -o output_file
+Options:
+    -koopa: Generate Koopa IR
+    -riscv: Generate RISC-V assembly
+    -perf: Performance test
+
+If no input file is specified, read from stdin
+If no output file is specified, write to stdout
+"#;
+
+impl Cli {
+    pub fn parse() -> Self {
+        let mode: Mode;
+        let input_file ;
+        let output_file;
+
+        if env::args().len() > 5 || env::args().len() == 1 {
+            print!("{}", USAGE);
+            exit(1);
+        }
+
+        let args: Vec<String> = env::args().collect();
+        match args[1].as_str() {
+            "-koopa" => {
+                mode = Mode::Koopa;
+            }
+            "-riscv" => {
+                mode = Mode::Riscv;
+            }
+            "-perf" => {
+                mode = Mode::Perf;
+            }
+            _ => {
+                print!("{}", USAGE);
+                exit(1);
+            }
+        }
+
+        if args.len() == 2
+        {
+            input_file = None;
+            output_file = None;
+        
+        }
+        else if args.len() == 3 {
+            input_file = Some(args[2].clone());
+            output_file = None;
+        }
+        else if args.len() == 4 {
+            print!("{}", USAGE);
+            exit(1);
+        }
+        else
+        {
+            assert!(args.len() == 5);
+            input_file = Some(args[2].clone());
+            if args[3] != "-o" {
+                print!("{}", USAGE);
+                exit(1);
+            }
+            output_file = Some(args[4].clone());
+        }
+        
+        Self
+        {
+            mode,
+            input_file,
+            output_file,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
-fn main() {
-    println!("Hello, world!");
+mod lexer;
+
+mod cli;
+use cli::Cli;
+use lexer::Token;
+use logos::Logos;
+use std::{io::{Read, Write}};
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = Cli::parse();
+    let mut istream: Box<dyn Read>;
+    let mut ostream: Box<dyn Write>;
+    match cli.input_file {
+        Some(file) => {
+            istream = Box::new(std::fs::File::open(file)?);
+        }
+        None => {
+            istream = Box::new(std::io::stdin());
+        }
+    }
+    match cli.output_file {
+        Some(file) => {
+            ostream = Box::new(std::fs::File::create(file)?);
+        }
+        None => {
+            ostream = Box::new(std::io::stdout());
+        }
+    }
+
+
+    match cli.mode {
+        cli::Mode::Koopa => {
+            excute_koopa(istream.as_mut(), ostream.as_mut())
+        }
+        cli::Mode::Riscv => {
+            return Err("Riscv mode is not implemented yet".into());
+        }
+        cli::Mode::Perf => {
+            return Err("Perf mode is not implemented yet".into());
+        }
+    }
+}
+
+fn excute_koopa(istream: &mut dyn Read, ostream: &mut dyn Write) -> Result<(), Box<dyn std::error::Error>>
+{
+    let mut input = String::new();
+    istream.read_to_string(&mut input)?;
+    
+    let mut lexer_result = Token::lexer(&input);
+    let mut tokens = Vec::new();
+    while let Some(token) = lexer_result.next() {
+        match token {
+            Err(_) => {
+                eprintln!("Lexer Result: {:?}", Token::lexer(&input).collect::<Vec<_>>());
+                return Err("Lexer error".into());
+            }
+            Ok(t) => {
+                tokens.push(t);
+            }
+        }
+    }
+    println!("Lexer Result: {:?}", tokens);
+    Ok(())
 }
